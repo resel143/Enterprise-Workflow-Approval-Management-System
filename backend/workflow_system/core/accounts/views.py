1438dad0df3caa7fb7ba4a30546ee25938bf2cbc
Reshul Wate <reshulwate@gmail.com>
2026-01-18 14:16:12 +0530

    Feat: Register API

--- backend/workflow_system/core/accounts/views.py
+++ backend/workflow_system/core/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': "User Registration Success!",
+            }, status=status.HTTP_200_OK)
+        
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
